2b5ca7a613f2c7648ba897e410cdfafde64b8bb9
UzairBinShaukat <uzairbinshaukat@gmail.com>
2020-07-02 01:13:50 -0700

    code minification in navbar.js and counters.js

--- src/components/counters.js
+++ src/components/counters.js
@@ -1,26 +1,21 @@
 import React, { Component } from "react";
 import Counter from "./counter";
-class Counters extends Component {
-  render() {
-    return (
-      <div>
-        <button
-          className="btn btn-primary btn-sm m-2"
-          onClick={this.props.onReset}
-        >
-          Reset
-        </button>
-        {this.props.counters.map((counter) => (
-          <Counter
-            key={counter.id}
-            counter={counter}
-            onIncrement={this.props.onIncrement}
-            onDecrement={this.props.onDecrement}
-          />
-        ))}
-      </div>
-    );
-  }
-}
+const Counters = (props) => {
+  return (
+    <div>
+      <button className="btn btn-primary btn-sm m-2" onClick={props.onReset}>
+        Reset
+      </button>
+      {props.counters.map((counter) => (
+        <Counter
+          key={counter.id}
+          counter={counter}
+          onIncrement={props.onIncrement}
+          onDecrement={props.onDecrement}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Counters;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,25 +1,13 @@
-import React, { Component } from "react";
-class Navbar extends Component {
-  getTotalCounts = () => {
-    let a = 0;
-    for (let i = 0; i < this.props.counters.length; i++) {
-      a += this.props.counters[i].value;
-    }
-    console.log(a);
-    return a;
-  };
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar
-          <span className="badge badge-secondary m-2">
-            {this.props.counters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+import React from "react";
+const Navbar = (props) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar
+        <span className="badge badge-secondary m-2">{props.counters}</span>
+      </a>
+    </nav>
+  );
+};
 
 export default Navbar;
